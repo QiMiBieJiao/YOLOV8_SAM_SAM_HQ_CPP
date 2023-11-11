9139d2d4ad59be42bae99e43c15f939dafa1e343
what is this <843230255@qq.com>
2023-11-11 22:16:43 +0800

    yoloheader

---
+++ ortyolonet.h
@@ -0,0 +1,34 @@
+﻿#ifndef ORTYOLONET_H
+#define ORTYOLONET_H
+#include<baseortnet.h>
+class YOLOOrtNetPrivate;
+class ORTNET_EXPORT YOLOOrtNet:public BaseOrtNet
+{
+public:
+    enum Type
+    {
+        Classify,
+        Detect,
+        Pose,
+        Segment,
+        FastSAM
+    };
+    struct Task
+    {
+        Type taskType;
+        int classCount=-1;
+        std::vector<int64_t> kptShape;
+    };
+    YOLOOrtNet(const Task &cfg);
+    Task getTask();
+    std::vector<NetOutputResult> predict(const cv::Mat &src,const PromptFilter &filter);
+    std::vector<std::vector<NetOutputResult>> predictBatch(const std::vector<cv::Mat> &srcVec,const PromptFilter &filter);
+protected:
+    virtual bool netSanityCheck();;
+private:
+    std::unique_ptr<YOLOOrtNetPrivate> d;
+    friend class YOLOOrtNetPrivate;
+
+};
+
+#endif // ORTYOLONET_H
